f63277d968745cb31554e2d6698abac7375fe1c5
OmkaarRege <omkaarrege1@gmail.com>
2025-02-20 11:05:05 -0500

    Working on creature class

--- Synthesis/Assets/Scripts/Creatures/Creature.cs
+++ Synthesis/Assets/Scripts/Creatures/Creature.cs
@@ -6,6 +6,14 @@ namespace Synthesis.Creatures
 {
     public class Creature : MonoBehaviour
     {
+        public float hp,strength,attack1damage, attack2damage, strengthincrease1, strengthincrease2;
+        public string attack1, attack2, strength1, strength2;
+
+       
+
 
     }
+
+   
+    
 }
